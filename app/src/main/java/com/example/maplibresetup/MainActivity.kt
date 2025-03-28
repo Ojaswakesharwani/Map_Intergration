e4fb89c1f3d53711debdf99b16871233b47674b0
Ojaswakesharwani <ojaswakesharwani@gmail.com>
2025-03-29 03:07:38 +0530

    Add different style of map

--- app/src/main/java/com/example/maplibresetup/MainActivity.kt
+++ app/src/main/java/com/example/maplibresetup/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.maplibresetup
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import org.maplibre.android.MapLibre
 import org.maplibre.android.camera.CameraPosition
@@ -21,11 +22,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(rootView)
 
         mapView = rootView.findViewById(R.id.mapView)
-        mapView.getMapAsync { map ->
-            map.setStyle("https://demotiles.maplibre.org/style.json")
-            map.cameraPosition = CameraPosition.Builder().target(LatLng(0.0, 0.0)).zoom(1.0).build()
+
+        setupMap("https://demotiles.maplibre.org/style.json")
+        findViewById<Button>(R.id.btn_street).setOnClickListener {
+            setupMap("https://api.maptiler.com/maps/streets-v2/style.json?key=gXws6DRBGz2gvXFIxuFH")
+        }
+
+        findViewById<Button>(R.id.btn_demo).setOnClickListener {
+            setupMap("https://demotiles.maplibre.org/style.json")
+
+        }
+
+        findViewById<Button>(R.id.btn_aquarelle).setOnClickListener {
+            setupMap("https://api.maptiler.com/maps/aquarelle/style.json?key=gXws6DRBGz2gvXFIxuFH")
+
+        }
+
+        findViewById<Button>(R.id.btn_satellite).setOnClickListener {
+            setupMap("https://api.maptiler.com/maps/satellite/style.json?key=gXws6DRBGz2gvXFIxuFH")
+
+        }
+
+        findViewById<Button>(R.id.btn_street_map).setOnClickListener {
+            setupMap("https://api.maptiler.com/maps/openstreetmap/style.json?key=gXws6DRBGz2gvXFIxuFH")
+
         }
     }
+
+       private fun setupMap(style : String){
+           mapView.getMapAsync { map ->
+               map.setStyle(style)
+               map.cameraPosition = CameraPosition.Builder().target(LatLng(0.0, 0.0)).zoom(1.0).build()
+           }
+       }
 
         override fun onStart() {
             super.onStart()
